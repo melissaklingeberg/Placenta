c6f81d27ccbc4a00478f164a9d08ff190fba46ea
melissaklingeberg <klingebergmelissa@gmail.com>
2022-07-04 14:52:43 +0200

    Modifiy code for publishing 1

--- Rawdata-cleanup.R
+++ Rawdata-cleanup.R
@@ -1,8 +1,6 @@
-## PLACENTA PROJECT - HPA :) 
+## PLACENTA PROJECT - ANALYSIS OF ANNOTATION DATA FROM IHC 
 
 ###    1. GENERAL SETUP ----
-
-  
 #### 1.1. Library ----
 library(dplyr)
 library(tidyr)
@@ -33,18 +31,23 @@ tropho_man <- read_excel("Placenta_Rawdata/troph_spec_manual.xlsx")
 gene_names <- read_excel("Placenta_Rawdata/Genes.xlsx")
 
 ###    2. DATA PREPERATION ----
-#### 2.1 Formatting ----
-Annotation_Rawdata[ Annotation_Rawdata == "NA" ] <- NA                #change character "NA" to NA
 
+#### 2.1 Formatting ----
+#change character "NA" to NA
+Annotation_Rawdata[ Annotation_Rawdata == "NA" ] <- NA                
 cleaned_data <- Annotation_Rawdata  %>%
+  #separate first column into four columns
   separate(identifiers,
            c("Observation", "Celltype", "ID", "Time"),
            sep = "_",
-           remove = TRUE) %>%                                         #separate first column into four columns
-  gather(Antibody, value, 5:ncol(.)) %>%                              #reshape "AB" columns to rows
-  spread(Observation, value) %>%                                      #spread "Observation" into 3 separate columns
+           remove = TRUE) %>% 
+  #reshape "AB" columns to rows
+  gather(Antibody, value, 5:ncol(.)) %>%    
+  #spread "Observation" into 3 separate columns
+  spread(Observation, value) %>%  
+  #change chr to num in columns + change format of Intensity
   mutate(Qt = as.numeric(Qt), 
-         Int = ifelse(as.numeric(Int)<=1, "Undistinct", "Distinct"))  #change chr to num in columns + change format of Intensity
+         Int = ifelse(as.numeric(Int)<=1, "Undistinct", "Distinct"))
 
 #### 2.2 Figure TMA ----
 meta_data %>%
@@ -61,30 +64,37 @@ meta_data %>%
         axis.line = element_blank()) 
 
 ###    3. QC TISSUE ----
-#### 3.1 Outliers ----
+#### 3.1 Find Outliers ----
+#count different Celltypes
 total_cell <- cleaned_data %>%
   count(Celltype) %>% nrow()
-
+#count number of Antobodies
 total_ab <- cleaned_data %>%
   count(Antibody) %>% nrow()
 
 tissue_QC <- cleaned_data %>%
-  group_by(ID, Antibody) %>%                                #summarize patients in a group
-  summarise(NA_count = sum(is.na(Int))) %>% ungroup() %>%   #count cores, where no cells (NA=4) could be found
+  #summarize patients in a group
+  group_by(ID, Antibody) %>%  
+  #count cores, where no cells (NA=4) could be found
+  summarise(NA_count = sum(is.na(Int))) %>% ungroup() %>%   
   filter(NA_count == total_cell) %>%
   count(ID) %>%  
-  mutate(Per_Missing = n/total_ab*100) %>%                  #create new column with percentage of empty cores, filter for those with more than 25% empty
+  #create new column with percentage of empty cores
+  mutate(Per_Missing = n/total_ab*100) %>%   
+  #filter for those with more than 25% empty (treshold)
   filter(Per_Missing > 25)
-
 
 #### 3.2 Figure QC Tissue ----
 TMA_Missing <- cleaned_data %>%
-  group_by(ID, Antibody) %>%                                #summarize patients in a group
-  summarise(NA_count = sum(is.na(Int))) %>% ungroup() %>%   #count cores, where no cells (NA=4) could be found
+  #summarize patients in a group
+  group_by(ID, Antibody) %>%    
+  #count cores, where no cells (NA=4) could be found
+  summarise(NA_count = sum(is.na(Int))) %>% ungroup() %>%   
   filter(NA_count == total_cell) %>%
   count(ID) %>%  
-  mutate(Per_Missing = n/total_ab*100) %>%                  #create new column with percentage of empty cores, filter for those with more than 25% empty
+  #create new column with percentage of empty cores
   full_join(meta_data, by = "ID") %>%
+  mutate(Per_Missing = n/total_ab*100) %>%                  
   replace_na(list(n = 0, Per_Missing = 0)) 
 
 TMA_Missing %>%
@@ -98,20 +108,27 @@ TMA_Missing %>%
         axis.ticks = element_blank(),
         axis.line = element_blank()) + 
   scale_colour_gradient(low = "#90CE90", high = "#FF99CC",limits = c(0, 100) ) 
+#export table
+write.table(tissue_QC, file = "Outlier_tissue.txt", sep = "\t")   
 
-write.table(tissue_QC, file = "Outlier_tissue.txt", sep = "\t")      #export table
-
-filtered_tissue <- cleaned_data %>%                                  #delete outlier(s) from table
+#### 3.3 Remove outlier(s) from table ----
+filtered_tissue <- cleaned_data %>%                                 
   filter(!ID %in% tissue_QC$ID) 
 
 ###    4. QC AB ----
-#### 4.1 Outliers ----
+
+#### 4.1 Find Outliers ----
 ab_QC_outliers <- filtered_tissue %>%
+  #summarize antibodies into a group
   group_by(Antibody,Int) %>%  
+  #count intensity values (NA, undist., distinct) for each AB
   summarise(count = n()) %>% 
+  #reshape table, 1 line per antibody
   spread(Int, count) %>% 
   rename(NAInt = "<NA>") %>%
+  # replace to numerical values for calculation
   replace_na(list(Distinct = 0, Undistinct = 0, NAInt = 0)) %>%
+  #treshold for AB to pass QC: at least 2 distinct stainings, define outliers
   filter(Distinct <2 )
 #export table
 write.table(ab_QC, file = "Outlier_ab.txt", sep = "\t")
@@ -125,12 +142,13 @@ ab_QC <- filtered_tissue %>%
   replace_na(list(Distinct = 0, Undistinct = 0, NAInt = 0)) %>%
   gather(AB_qual, "count", 2:4)
   
-  #Order Antibodys based on number of distinct staining
+#Order Antibodys based on number of distinct staining
 AB_order <- ab_QC %>%
   filter(AB_qual == "Distinct") %>% 
   arrange(count) %>%
   pull(Antibody) 
 
+#figure
 ab_QC %>%
   mutate(Antibody = factor(Antibody, levels = AB_order)) %>%
   ggplot(aes(x="", y=count, fill=AB_qual)) +
@@ -144,14 +162,15 @@ ab_QC %>%
       ) +
   facet_wrap(~ Antibody) 
  
+#### 4.3 Remove Outliers ----
 filter_tiss_ab <- filtered_tissue %>% 
   filter(!Antibody %in% ab_QC_outliers$Antibody)
-
 #export final table with data cleaned for tissue & AB
 write.table(filter_tiss_ab, file = "superclean.txt", sep = "\t")
 
 ### 5. TROPHO SPEC ----
-#### 5.1 Outliers ----
+#### 5.1 FindOutliers ----
+# remove antibodies, that are not spec. for trophoblasts (= stain in DC)
 tropho_unspec <- filter_tiss_ab %>%
   filter(Celltype == "DC") %>%
   count(Antibody, Int) %>% 
@@ -159,86 +178,78 @@ tropho_unspec <- filter_tiss_ab %>%
 #export table
 write.table(tropho_unspec, file = "tropho_unspec.txt", sep = "\t")
 
+#### 5.2 Remove Outliers ----
 tropho_spec <- filter_tiss_ab %>% 
   filter(!Antibody %in% tropho_unspec$Antibody) %>%
   distinct(Antibody) 
- 
-#### 5.2 Export ----
 #export final table with data cleaned for tissue & AB
 write.table(tropho_spec, file = "tropho_spec.txt", sep = "\t", row.names = FALSE)
 
-### 6. Manual annotation - trophoblast specificity ----
+#### 5.4 Incorporate Manual Annotation Data ----
 tspec_raw <- filter_tiss_ab %>%
+# keep only antibodies, that are tropho-specific in both lists
 inner_join(tropho_man, by = "Antibody")
 
-### 7. AB - Celltype specificity ----
-#### 7.1 Filtering ----
+### 6. FIGURES: TROPHOBLAST MARKER  ----
+#### 6.1 Venn Diagram - Trophoblast specific marker ----
+#Filtering for trophoblast-spec antibodies 
 AB_Venn <- tspec_raw %>% 
   filter(Int == "Distinct") %>%
   group_by(Antibody, Celltype) %>%
   count(Int)
-  
-#### 7.2 Venn Diagram ----
+# create lists Venn
 set_SCT <- AB_Venn %>% filter(Celltype == "SCT") %>% pull(Antibody)
 set_CT <- AB_Venn %>% filter(Celltype == "CT") %>% pull(Antibody)
 set_EVT <- AB_Venn %>% filter(Celltype == "EVT") %>% pull(Antibody)
-
+# Venn plot
 grid.newpage()   
 myV <- plotVenn(list(SCT = set_SCT, CT = set_CT, EVT= set_EVT), nCycles = 2000 )
 showSVG(myV, opacity = 0.3, borderWidth = 2 , outFile = "Venn.tiff", labelRegions = FALSE,
         setColor = c("#99CCFF", "#CC99FF","#FFCC99"))
 
-#### 7.3 Upset Plot ----
+#### 6.2 Upset Plot - Trophoblast specific marker ----
+# create lists 
 set_SCT <- AB_Venn %>% filter(Celltype == "SCT") %>% pull(Antibody)
 set_CT <- AB_Venn %>% filter(Celltype == "CT") %>% pull(Antibody)
 set_EVT <- AB_Venn %>% filter(Celltype == "EVT") %>% pull(Antibody)
 ElevatedGenes <- list("SCT" = set_SCT, "CT" = set_CT, "EVT"= set_EVT)
-
+# Upset Plot
 upset(
   fromList(ElevatedGenes),
   c("SCT","CT", "EVT"),
   queries=list(
     upset_query(set="EVT", fill='#FFCC99'),
     upset_query(set="CT", fill='#CC99FF'),
-    upset_query(set="SCT", fill='#99CCFF')
-    ),
+    upset_query(set="SCT", fill='#99CCFF')),
   base_annotations=list(
     'trophoblast genes'=(
       intersection_size(
-        bar_number_threshold=1,  # show all numbers on top of bars
-        width=0.5,# reduce width of the bars
-      )
+        # show all numbers on top of bars
+        bar_number_threshold=1, 
+        # reduce width of the bars
+        width=0.5,)
       + theme(
         # hide grid lines
         panel.grid.major=element_blank(),
         panel.grid.minor=element_blank(),
         # show axis lines
-        axis.line=element_line(colour='black')
-      )
-    )
-  ),
+        axis.line=element_line(colour='black')))),
   stripes=upset_stripes(
     geom=geom_segment(size=12),  
-    colors=c('grey95', 'white')
-  ),
+    colors=c('grey95', 'white')),
   matrix=intersection_matrix(
     geom=geom_point(
       shape='circle filled',
       size=3.5,
-      stroke=0.3
-    )
-  ),
+      stroke=0.3)),
   set_sizes=(
     upset_set_size(geom=geom_bar(width=0.6))
     + theme(
       axis.line.x=element_line(colour='black'),
-      axis.ticks.x=element_line()
-    )
-  ),
-  sort_sets='FALSE'
-)
+      axis.ticks.x=element_line())),
+  sort_sets='FALSE')
 
-#### 8. MARKER PROTEINS FOR SPECIFIC TIMEPOINT OF PLACENTAL DEVELOPMENT ----
+#### 6.3 Lineplot - Timepoint specific marker ----
 tspec_raw <- cleaned_data %>%
   inner_join(tropho_man, by = "Antibody") %>%
   filter(ID != 420.1, 
@@ -253,14 +264,3 @@ tspec_raw %>%
   geom_point() +
   geom_smooth(se=FALSE) +
   facet_wrap(~Gene.x)
-
-
-
-citation("tidyverse")
-
-
-
-
-
-
-
